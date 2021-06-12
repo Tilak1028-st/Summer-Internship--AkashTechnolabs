a78c5174fed8fd25146686aa0e674f899a2c5acd
Shakya Tilak <75114840+Tilak1028-st@users.noreply.github.com>
2021-06-12 12:22:39 +0530

    Add files via upload

---
+++ DAY 7/ListView/ListActivity.java
@@ -0,0 +1,34 @@
+package com.example.webactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class ListActivity extends AppCompatActivity {
+
+    ListView listView;
+    String course[] = {"Android", "Php", "Ios", "Angular Js", "Ionic", "Wordpress", "Java", "c#", "Html",
+            "C++", "Node Js","Flutter"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        listView=findViewById(R.id.lvCourse);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,course);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),course[position],Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
